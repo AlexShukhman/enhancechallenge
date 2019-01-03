9384e02b532a3e92cd0c40871d1ed71588e1b0a1
alexshukhman <alex.jacob.shukhman@gmail.com>
2019-01-02 22:51:21 -0500

    installed beautifulsoup

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-import bs4
+'''
+
+Enhance Coding Challenge
+Alex Shukhman
+
+'''
+# Imports
+from bs4 import BeautifulSoup as bs
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import urlopen
+
+page = urlopen('http://google.com')
+
+soup = bs(page, 'html.parser')
+
+body = soup.find('body').prettify()
+
+print(body)
